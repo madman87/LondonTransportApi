25c0dffbb20d1b3efbcd2a0657c5b09dfdca19f0
Roman <itexpress@bk.ru>
2021-11-08 16:30:27 +0100

    update

--- static/script.js
+++ static/script.js
@@ -3,43 +3,43 @@
 const callapiBtn = document.getElementById('callapi'),
       result = document.getElementById("result").querySelector("tbody");
 
-if (callapiBtn) {callapiBtn.addEventListener('click', async (e) => {
-    
-    const response = await fetch('https://api.tfl.gov.uk/StopPoint/490009333W/arrivals');
-    const myJson = await response.json(); //extract JSON from the http response
+if (callapiBtn) {
+    callapiBtn.addEventListener('click', async (e) => {
 
-    // console.log(Object.keys(myJson));
-    result.innerHTML = "<tr><td colspan='5'>LOADING</td></tr>";
+        const response = await fetch('https://api.tfl.gov.uk/StopPoint/490009333W/arrivals');
+        const myJson = await response.json(); //extract JSON from the http response
 
-    let trarray = "";
-    for (let i = 0; i < myJson.length; i++) {
-        let r = myJson[i];
-        console.log(myJson[i]);
-        let time = myJson[i].expectedArrival;
-        let destination = myJson[i].destinationName;
-        let station = myJson[i].stationName;
-        let vehicle = myJson[i].vehicleId;
-        let tr = createTr(vehicle,time, station, destination);
-        trarray += tr;
-    }
-    result.innerHTML = trarray;
-    
-    //SEND CALL API DATA TO BACKEND
-    const data = new FormData(); //FORM DATA SEND
-    data.append("json", JSON.stringify(myJson));
+        result.innerHTML = "<tr><td colspan='5'>LOADING</td></tr>";
 
-    fetch("/getdata",
-        {
-            method: "POST",
-            body: data,
-        })
-        .then(function (res) {
-            return res.json();
-        }).catch((error) => {
-            console.log(error);
-        });
-    
-});}
+        let trarray = "";
+        for (let i = 0; i < myJson.length; i++) {
+            let r = myJson[i];
+            console.log(myJson[i]);
+            let time = myJson[i].expectedArrival;
+            let destination = myJson[i].destinationName;
+            let station = myJson[i].stationName;
+            let vehicle = myJson[i].vehicleId;
+            let tr = createTr(vehicle, time, station, destination);
+            trarray += tr;
+        }
+        result.innerHTML = trarray;
+
+        //SEND CALL API DATA TO BACKEND
+        const data = new FormData(); //FORM DATA SEND
+        data.append("json", JSON.stringify(myJson));
+
+        fetch("/getdata",
+            {
+                method: "POST",
+                body: data,
+            })
+            .then(function (res) {
+                return res.json();
+            }).catch((error) => {
+                console.log(error);
+            });
+    });
+}
 
 function createTr(v, t, s, d) {
     return "<tr><td>" + v + "</td><td>" + t + "</td><td>" + s + "</td><td>" + d + "</td></tr>";
@@ -47,12 +47,15 @@ function createTr(v, t, s, d) {
 
 const deleteBtn = document.querySelectorAll('.row_entry'),
       heading = document.querySelector('.heading');
+let c=1;
 
 deleteBtn.forEach(item => {
-    heading.innerHTML=(`<h3> ${deleteBtn.length} Entries Found </h3>`);
+    heading.innerHTML = (`<h3> ${deleteBtn.length} Entries Found </h3>`);
     item.addEventListener("click", (e) => {
         if (e.target.matches('.img')) {
             let objectNum = item.dataset.objectid;
+            heading.innerHTML = (`<h3> ${deleteBtn.length-c} Entries Found </h3>`);
+            c ++;
 
             //SEND CALL API ELEMENT ID NUMBER TO BACKEND TO DELETE FROM DB        
             fetch("/delelement/" + JSON.stringify(objectNum),
@@ -60,14 +63,14 @@ deleteBtn.forEach(item => {
                     method: "POST",
                 })
                 .then(function (res) {
-                    if (res.status == 200){
-                        let row =e.target.parentElement.parentElement;
+                    if (res.status == 200) {
+                        let row = e.target.parentElement.parentElement;
                         row.remove();
                     }
                     return res.json();
                 }).catch((error) => {
                     console.log(error);
-                });      
+                });
         }
     });
 });
